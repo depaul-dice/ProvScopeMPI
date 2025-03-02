841a0073f2abaa0ef347e33645597280b8c5a852
Yuta Nakamura <yutanak6@gmail.com>
2025-03-01 15:57:10 -1000

    adding line to debug

--- share/alignment.cpp
+++ share/alignment.cpp
@@ -980,6 +980,7 @@ void appendTraces(
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     //static unsigned long index = 0;
     unsigned long index = 0;
+    cerr << rawTraces.size() << rank << endl;
     if(traces.size() == 0) {
         //MPI_ASSERT(index == 0);
         traces = makeHierarchyMain(
